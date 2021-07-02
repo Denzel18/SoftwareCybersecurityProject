bfdbff95dbb8a774bc35564922f0dfeca9a89ad4
LorenzoFratini <s1083514@studenti.univpm.it>
2021-07-02 12:45:08 +0200

    Aggiunto modulo connessione mysql e creazione database "cybersecurity"

---
+++ webapp/connect.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    password: 'user',
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+    connection.query("CREATE DATABASE cybersecurity", function (err, result) {
+        if (err) throw err;
+        console.log("Database created");
+    });
+});
+//module.exports=connection;
--- webapp/routes/users.js
+++ webapp/routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const connection = require('../connect')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  //TODO
 });
 
 module.exports = router;
