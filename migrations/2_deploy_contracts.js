9d46286c6912d64901959e74b6fa308e38de12fc
Denis Bernovschi <bernovschi.denis@gmail.com>
2021-07-26 11:41:08 +0200

    Truffle OK v.4.1.16

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -25,3 +25,49 @@ module.exports = async function (deployer) {
 
     //await Contract.insertMany(contracts);
 };
+
+
+
+// let Biglietti = artifacts.require("./Biglietti.sol");
+// let Evento = artifacts.require("./Evento.sol");
+// const Web3 = require("web3");
+// const Contract = require("../webapp/models/Contract");
+// const config = require("../webapp/config");
+// const Sequelize = require("sequelize");
+
+// module.exports = async (deployer) => {
+//   let web3 = new Web3("http://localhost:22001");
+//   let accounts = await web3.eth.getAccounts();
+//   console.log(accounts);
+
+//   const database = new Sequelize('cybersecurity', 'user', 'user', {
+//     dialect: 'mysql',
+//     host: "localhost",
+//     port: 3306,
+//   })
+
+// //   database.query("TRUNCATE TABLE Contract'", {type: database.QueryTypes.TRUNCATE}).then(results=>{
+// //     console.log(results);
+// //     });
+
+//   await Contract.deleteMany({});
+//   await deployer.deploy(Evento,accounts[0]);
+//   await deployer.deploy(Biglietti, Evento.address, accounts[0]);
+  
+//   let EE = await Evento.deployed();
+//   let EB = await Biglietti.deployed();
+  
+
+// //   let contracts = [{
+// //     name: "Evento",
+// //     address: EE.address
+// //   }, {
+// //     name: "Biglietti",
+// //     address: EB.address
+// //   }];
+
+// //   await Contract.insertMany(contracts);
+  
+//   console.log("INDIRIZZO EVENTO : " + EE.address)
+//   console.log("INDIRIZZO BIGLIETTI : " + EB.address)
+// };
